646e405f2e169cc99fc95c8520da1268dd4a3118
Gaetan Bahl <gaetan.bahl@gmail.com>
2022-12-15 10:59:48 +0100

    day5 python

---
+++ python/day_05/part1.py
@@ -0,0 +1,27 @@
+stacks = [[] for i in range(9)]
+
+with open("input.txt", 'r') as f:
+    lines = f.readlines()
+
+letter_indices = [1 + 4*i for i in range(9)]
+
+for l in reversed(lines[:8]):
+    for i in range(9):
+        letter = l[letter_indices[i]]
+        if letter != " ":
+            stacks[i].append(letter)
+
+moves = []
+for l in lines[10:]:
+    l = l.split(" ")
+    moves.append((int(l[1]), int(l[3]), int(l[5])))
+
+for m in moves:
+    for i in range(m[0]):
+        c = stacks[m[1]-1].pop()
+        stacks[m[2]-1].append(c)
+
+for s in stacks:
+    print(s[-1], end="")
+
+print()
