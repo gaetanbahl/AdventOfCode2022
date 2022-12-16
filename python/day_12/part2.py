f582511f28012573780ce484ccc2ac68da14e0d6
Gaetan Bahl <gaetan.bahl@gmail.com>
2022-12-17 00:46:44 +0100

    python day 12

---
+++ python/day_12/part2.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+with open("input.txt", 'r') as f:
+    lines = f.readlines()
+    lines = [l.strip() for l in lines]
+
+shape = (len(lines), len(lines[0]))
+
+array = np.zeros(shape)
+start = [0,0]
+end = [0,0]
+
+for i,l in enumerate(lines):
+    for j,c in enumerate(l):
+        array[i, j] = ord(c) - 97
+        if c == "S":
+            start = (i,j)
+        elif c == 'E':
+            end = (i,j)
+
+print("starting pos:", start)
+print("end pos:", end)
+
+array[start[0], start[1]] = 0
+array[end[0], end[1]] = 25
+
+def possibilities(i,j,array):
+    poss = []
+    if j > 0:
+        if array[i, j-1] - array[i, j] <= 1:
+            poss.append((i, j-1))
+    if i > 0:
+        if array[i-1, j] - array[i, j] <= 1:
+            poss.append((i-1, j))
+    if j < array.shape[1] - 1:
+        if array[i, j+1] - array[i, j] <= 1:
+            poss.append((i, j+1))
+    if i < array.shape[0] - 1:
+        if array[i+1, j] - array[i, j] <= 1:
+            poss.append((i+1, j))
+
+    return poss
+
+all_results = []
+
+for i in range(array.shape[0]):
+    for j in range(array.shape[1]):
+    
+        if array[i, j] != 0:
+            continue
+
+        start = (i, j)
+
+        lengths = np.zeros(shape) + np.inf
+        lengths[start[0], start[1]] = 0
+
+        stack = [start]
+        visited = set()
+
+        while len(stack):
+            curr = stack.pop(0)
+            dist = lengths[curr[0], curr[1]]
+            visited.add(curr)
+            candidates = [p for p in possibilities(curr[0], curr[1], array) if p not in visited]
+            for c in candidates:
+                lengths[c[0], c[1]] = min(dist+1, lengths[c[0], c[1]])
+                if not c in stack:
+                    stack.append(c)
+            stack = sorted(stack, key=lambda x : lengths[x[0], x[1]])
+
+        all_results.append(lengths[end[0], end[1]])
+
+print(sorted(all_results)[0])
